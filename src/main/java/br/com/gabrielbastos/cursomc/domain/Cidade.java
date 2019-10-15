8cca2bbfac44ae40666ad2a5fda5886736160cc8
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2019-10-15 18:37:36 -0300

    Atualização utilizando somente JsonIgnore

--- src/main/java/br/com/gabrielbastos/cursomc/domain/Cidade.java
+++ src/main/java/br/com/gabrielbastos/cursomc/domain/Cidade.java
@@ -10,7 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cidade implements Serializable {
@@ -23,7 +22,6 @@ public class Cidade implements Serializable {
 	private Integer id;
 	private String nome;
 
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
--- src/main/java/br/com/gabrielbastos/cursomc/domain/Cliente.java
+++ src/main/java/br/com/gabrielbastos/cursomc/domain/Cliente.java
@@ -14,7 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.gabrielbastos.cursomc.domain.enums.TipoCliente;
 
@@ -31,7 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
 
@@ -39,7 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "telefone")
 	private Set<String> telefones = new HashSet<String>();
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<Pedido>();
 
