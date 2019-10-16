ec2a776e749ccca9e8394f08401cb9ad0862b1b5
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2019-10-16 00:55:46 -0300

    Validação sintatica com bean validation

--- src/main/java/br/com/gabrielbastos/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/gabrielbastos/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.gabrielbastos.cursomc.domain.Categoria;
+import br.com.gabrielbastos.cursomc.dto.CategoriaDTO;
 import br.com.gabrielbastos.cursomc.repositories.CategoriaRepository;
 import br.com.gabrielbastos.cursomc.services.exception.ObjectNotFoundException;
 
@@ -46,6 +47,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String directions){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(directions), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO obj) {
+		return new Categoria(obj.getId(), obj.getNome());
 	}
 	
 
