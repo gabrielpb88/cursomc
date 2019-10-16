cd4063c7cd861254e73a93848f2a0728377e9090
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2019-10-15 22:32:14 -0300

    Atualizando Categoria com PUT

--- src/main/java/br/com/gabrielbastos/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/gabrielbastos/cursomc/resources/CategoriaResource.java
@@ -24,7 +24,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		return ResponseEntity.ok(service.buscar(id));
+		return ResponseEntity.ok(service.find(id));
 	}
 	
 	@PostMapping
@@ -32,5 +32,12 @@ public class CategoriaResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
