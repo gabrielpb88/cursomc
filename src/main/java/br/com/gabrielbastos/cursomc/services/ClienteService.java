89d604e53dd389125fba9593464f1c77eeb066f6
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2019-12-20 18:14:34 -0300

    PUT, DELETE E GET para Cliente

--- src/main/java/br/com/gabrielbastos/cursomc/services/ClienteService.java
+++ src/main/java/br/com/gabrielbastos/cursomc/services/ClienteService.java
@@ -1,11 +1,17 @@
 package br.com.gabrielbastos.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.gabrielbastos.cursomc.domain.Cliente;
+import br.com.gabrielbastos.cursomc.dto.ClienteDTO;
 import br.com.gabrielbastos.cursomc.repositories.ClienteRepository;
 import br.com.gabrielbastos.cursomc.services.exception.ObjectNotFoundException;
 
@@ -13,12 +19,54 @@ import br.com.gabrielbastos.cursomc.services.exception.ObjectNotFoundException;
 public class ClienteService {
 	
 	@Autowired
-	private ClienteRepository clienteRepository;
+	private ClienteRepository repo;
 	
 	public Cliente buscar(Integer id) {
-		Optional<Cliente> obj = clienteRepository.findById(id);
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente cliente = find(obj.getId());
+		updateData(cliente, obj);
+		return repo.save(cliente);
+	}
+	
+	private void updateData(Cliente cliente, Cliente obj) {
+		cliente.setNome(obj.getNome());
+		cliente.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String directions){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(directions), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO obj) {
+		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null);
+	}
 
 }
