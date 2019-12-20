91428eef743755fcb24b3c25b41c1b935a69ba3b
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2019-12-20 18:23:34 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/br/com/gabrielbastos/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/gabrielbastos/cursomc/services/CategoriaService.java
@@ -31,10 +31,15 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
 		return repo.save(obj);
 	}
 	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+
 	public void delete(Integer id) {
 		find(id);
 		repo.deleteById(id);
