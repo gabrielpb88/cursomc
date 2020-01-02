00b87e67fb468af4e801d6586678a980c74a5294
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-01-02 17:21:42 -0300

    Busca de produtos por nome e categorias

--- src/main/java/br/com/gabrielbastos/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/br/com/gabrielbastos/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,20 @@
 package br.com.gabrielbastos.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import br.com.gabrielbastos.cursomc.domain.Categoria;
 import br.com.gabrielbastos.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
+	@Query("select distinct obj from Produto obj inner join obj.categorias cat where obj.nome like %:nome% and cat in :categorias")
+	Page<Produto> search(@Param("nome") String nome, @Param("categorias")List<Categoria> ids, Pageable pageRequest);
 }
