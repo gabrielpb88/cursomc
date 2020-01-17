05ae73d0b25b78b57cb8f6229207ce5badc71129
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-01-17 19:09:17 -0300

    Implementando autorizacao

--- src/main/java/br/com/gabrielbastos/cursomc/config/SecurityConfig.java
+++ src/main/java/br/com/gabrielbastos/cursomc/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.gabrielbastos.cursomc.security.JWTAuthenticationFilter;
+import br.com.gabrielbastos.cursomc.security.JWTAuthorizationFilter;
 import br.com.gabrielbastos.cursomc.security.JWTUtil;
 
 @Configuration
@@ -47,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
