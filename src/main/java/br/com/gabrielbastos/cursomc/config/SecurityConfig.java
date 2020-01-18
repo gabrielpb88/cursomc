260b68f10785dbf5982577d714436eff1a569cbe
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-01-18 12:20:48 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/br/com/gabrielbastos/cursomc/config/SecurityConfig.java
+++ src/main/java/br/com/gabrielbastos/cursomc/config/SecurityConfig.java
@@ -37,6 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
 
+	private static String[] PUBLIC_MATCHERS_POST = { "/clientes/"};
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -46,7 +48,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+				.antMatchers(PUBLIC_MATCHERS).permitAll()
+				.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+				.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
